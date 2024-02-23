ccaf804c5c6f95c28dc9bbf8045ca4ef8b4df1a8
Enzo Francesca <enzo.francesca@holomask.eu>
2024-02-23 12:23:12 +0100

    Mac adaptation

---
+++ applychanges.sh
@@ -0,0 +1,43 @@
+# andyshea folder
+cp ./src/changes/andyshea/ReactNativeUsbModule.txt ./node_modules/react-native-usb/android/src/main/java/me/andyshea/ReactNativeUsbModule.java 
+cp ./src/changes/andyshea/index.txt ./node_modules/react-native-usb/index.js 
+# react-native-webrtc-root
+cp ./src/changes/ReactNativeWebRTCRoot/index.txt ./node_modules/react-native-webrtc/src/index.ts
+cp ./src/changes/ReactNativeWebRTCRoot/MediaDevices.txt ./node_modules/react-native-webrtc/src/MediaDevices.ts
+cp ./src/changes/ReactNativeWebRTCRoot/showTextMessage.txt ./node_modules/react-native-webrtc/src/showTextMessage.ts
+cp ./src/changes/ReactNativeWebRTCRoot/showLoopBackCamera.txt ./node_modules/react-native-webrtc/src/showLoopBackCamera.ts
+cp ./src/changes/ReactNativeWebRTCRoot/showPointer.txt ./node_modules/react-native-webrtc/src/showPointer.ts
+cp ./src/changes/ReactNativeWebRTCRoot/showBitmap.txt ./node_modules/react-native-webrtc/src/showBitmap.ts
+cp ./src/changes/ReactNativeWebRTCRoot/showDisplay.txt ./node_modules/react-native-webrtc/src/showDisplay.ts
+cp ./src/changes/ReactNativeWebRTCRoot/heartBeat.txt ./node_modules/react-native-webrtc/src/heartBeat.ts
+# webRTCModule
+cp ./src/changes/WebRTCModule/BatteryArrayValue.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/BatteryArrayValue.java
+cp ./src/changes/WebRTCModule/PointerArrayValue.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/PointerArrayValue.java
+cp ./src/changes/WebRTCModule/DeviceCdc.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DeviceCdc.java
+cp ./src/changes/WebRTCModule/DeviceFb.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DeviceFb.java
+cp ./src/changes/WebRTCModule/GetUserMediaImpl.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
+cp ./src/changes/WebRTCModule/Parameters.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/Parameters.java
+cp ./src/changes/WebRTCModule/UnivetUtils.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UnivetUtils.java
+cp ./src/changes/WebRTCModule/UsbCapturer.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbCapturer.java
+cp ./src/changes/WebRTCModule/UsbException.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbException.java
+cp ./src/changes/WebRTCModule/WebRTCModule.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+cp ./src/changes/WebRTCModule/DailyAudioManager.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DailyAudioManager.java 
+cp ./src/changes/WebRTCModule/DailyWebRTCDevicesManager.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DailyWebRTCDevicesManager.java
+cp ./src/changes/WebRTCModule/UvcCapturer.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UvcCapturer.java
+cp ./src/changes/WebRTCModule/UvcEnumerator.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UvcEnumerator.java
+cp ./src/changes/WebRTCModule/CameraCaptureController.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/CameraCaptureController.java
+cp ./src/changes/WebRTCModule/buildgradle.txt ./node_modules/react-native-webrtc/android/build.gradle
+# USBMONITOR
+cp ./src/changes/WebRTCModule/CharsetsUtils.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/CharsetsUtils.java
+cp ./src/changes/WebRTCModule/Const.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/Const.java
+cp ./src/changes/WebRTCModule/ContextUtils.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/ContextUtils.java
+cp ./src/changes/WebRTCModule/DeviceFilter.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DeviceFilter.java
+cp ./src/changes/WebRTCModule/HandlerThreadHandler.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/HandlerThreadHandler.java
+cp ./src/changes/WebRTCModule/ResourceHelper.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/ResourceHelper.java
+cp ./src/changes/WebRTCModule/UsbDeviceInfo.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbDeviceInfo.java
+cp ./src/changes/WebRTCModule/USBMonitor.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/USBMonitor.java
+cp ./src/changes/WebRTCModule/UsbUtils.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbUtils.java
+cp ./src/changes/WebRTCModule/UsbVendorId.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbVendorId.java
+cp ./src/changes/WebRTCModule/XmlHelper.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/XmlHelper.java
+cp ./src/changes/WebRTCModule/BuildCheck.txt ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/BuildCheck.java 
+# FINISHED!
---
+++ savechanges.sh
@@ -0,0 +1,44 @@
+# andyshea folder
+cp ./node_modules/react-native-usb/android/src/main/java/me/andyshea/ReactNativeUsbModule.java ./src/changes/andyshea/ReactNativeUsbModule.txt
+cp ./node_modules/react-native-usb/index.js ./src/changes/andyshea/index.txt
+# react-native-webrtc-root
+cp ./node_modules/react-native-webrtc/src/index.ts ./src/changes/ReactNativeWebRTCRoot/index.txt
+cp ./node_modules/react-native-webrtc/src/MediaDevices.ts ./src/changes/ReactNativeWebRTCRoot/MediaDevices.txt
+cp ./node_modules/react-native-webrtc/src/showTextMessage.ts ./src/changes/ReactNativeWebRTCRoot/showTextMessage.txt
+cp ./node_modules/react-native-webrtc/src/showLoopBackCamera.ts ./src/changes/ReactNativeWebRTCRoot/showLoopBackCamera.txt
+cp ./node_modules/react-native-webrtc/src/showPointer.ts ./src/changes/ReactNativeWebRTCRoot/showPointer.txt
+cp ./node_modules/react-native-webrtc/src/showBitmap.ts ./src/changes/ReactNativeWebRTCRoot/showBitmap.txt
+cp ./node_modules/react-native-webrtc/src/showDisplay.ts ./src/changes/ReactNativeWebRTCRoot/showDisplay.txt
+cp ./node_modules/react-native-webrtc/src/heartBeat.ts ./src/changes/ReactNativeWebRTCRoot/heartBeat.txt
+# webRTCModule
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/BatteryArrayValue.java ./src/changes/WebRTCModule/BatteryArrayValue.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/PointerArrayValue.java ./src/changes/WebRTCModule/PointerArrayValue.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DeviceCdc.java ./src/changes/WebRTCModule/DeviceCdc.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DeviceFb.java ./src/changes/WebRTCModule/DeviceFb.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java ./src/changes/WebRTCModule/GetUserMediaImpl.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/Parameters.java ./src/changes/WebRTCModule/Parameters.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UnivetUtils.java ./src/changes/WebRTCModule/UnivetUtils.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbCapturer.java ./src/changes/WebRTCModule/UsbCapturer.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbException.java ./src/changes/WebRTCModule/UsbException.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java ./src/changes/WebRTCModule/WebRTCModule.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DailyAudioManager.java ./src/changes/WebRTCModule/DailyAudioManager.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DailyWebRTCDevicesManager.java ./src/changes/WebRTCModule/DailyWebRTCDevicesManager.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UvcCapturer.java ./src/changes/WebRTCModule/UvcCapturer.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UvcEnumerator.java ./src/changes/WebRTCModule/UvcEnumerator.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/CameraCaptureController.java ./src/changes/WebRTCModule/CameraCaptureController.txt
+cp ./node_modules/react-native-webrtc/android/build.gradle ./src/changes/WebRTCModule/buildgradle.txt
+# USBMONITOR
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/CharsetsUtils.java ./src/changes/WebRTCModule/CharsetsUtils.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/Const.java ./src/changes/WebRTCModule/Const.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/ContextUtils.java ./src/changes/WebRTCModule/ContextUtils.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/DeviceFilter.java ./src/changes/WebRTCModule/DeviceFilter.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/HandlerThreadHandler.java ./src/changes/WebRTCModule/HandlerThreadHandler.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/ResourceHelper.java ./src/changes/WebRTCModule/ResourceHelper.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbDeviceInfo.java ./src/changes/WebRTCModule/UsbDeviceInfo.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/USBMonitor.java ./src/changes/WebRTCModule/USBMonitor.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbUtils.java ./src/changes/WebRTCModule/UsbUtils.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/UsbVendorId.java ./src/changes/WebRTCModule/UsbVendorId.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/XmlHelper.java ./src/changes/WebRTCModule/XmlHelper.txt
+cp ./node_modules/react-native-webrtc/android/src/main/java/com/oney/WebRTCModule/BuildCheck.java ./src/changes/WebRTCModule/BuildCheck.txt
+
+# FINISHED!
